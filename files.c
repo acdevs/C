3137b6c334f5d93aa57d20c23b5ddee2fc2e6887
acdevs <amanchandra.in@gmail.com>
2023-02-14 01:29:58 +0530

    files.c updates.

--- files.c
+++ files.c
@@ -1,25 +1,34 @@
 #include<stdio.h>
 
 int main(){
-    FILE *f = NULL;
-    f = fopen("test.txt", "r");
+    FILE *f = fopen("test.txt", "r+");
     
     if(f == NULL){
         printf("Error opening file");
         return 1;
     }
     
-    //writing...
-    // char str[25] = "Aman Chandra";  
-    // fputs("Hello World", f);
-    // fputs(str, f);
-
-    //reading...
-    char ch;
+    //reading char by char...
+    // char ch;
     // while((ch = fgetc(f)) != EOF){
     //     printf("%c", ch);
     // }
-    // printf("%s", fgets(f));
+
+    // reading line by line...
+    // char str[5];
+    // while(fgets(str, 5, f) != NULL){
+    //     printf("%s", str);
+    // }
+
+    // reading at once...
+    // char str[5];
+    // fgets(str, 5, f);
+    // printf("%s", str);
+
+    //writing...
+    char str[25] = "get lost!";  
+    fputs(str, f);
+
     fclose(f);
     return 0;
 }
